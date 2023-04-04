d3f3fd729e2cd178e8f489e1529a9d25fd343e27
vagiz.daudov <vagiz.0282@gmail.com>
2023-04-04 15:52:45 +0300

    C24

--- edit_commits.sh
+++ edit_commits.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-count=0
-N_to_squash="$(($1-1))"
+count=1
+N_to_squash="$(($1))"
 
 echo "Combining first $1 commits together..."
 
 while [ "$count" -le "$N_to_squash" ]
 do
-  count="$((count+1))"
   if [[ $count -ne 1 ]]
   then
     sed -i '' "$count s/pick/squash/g" "$2"
   fi
+  count="$((count+1))"
 done
